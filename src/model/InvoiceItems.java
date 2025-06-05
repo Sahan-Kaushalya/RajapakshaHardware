d2ee9d1ca4ca93add1c7e0fb300451f40b5f12d5
Yohan3128 <Yohansilva560@gmail.com>
2025-06-05 14:23:43 +0530

    Yohan

---
+++ src/model/InvoiceItems.java
@@ -0,0 +1,87 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author Yohan Silva
+ */
+public class InvoiceItems {
+    private String stockId;
+    private String productId;
+    private String productName;
+    private double qty;
+    private double price;
+
+    /**
+     * @return the stockId
+     */
+    public String getStockId() {
+        return stockId;
+    }
+
+    /**
+     * @param stockId the stockId to set
+     */
+    public void setStockId(String stockId) {
+        this.stockId = stockId;
+    }
+
+    /**
+     * @return the productId
+     */
+    public String getProductId() {
+        return productId;
+    }
+
+    /**
+     * @param productId the productId to set
+     */
+    public void setProductId(String productId) {
+        this.productId = productId;
+    }
+
+    /**
+     * @return the productName
+     */
+    public String getProductName() {
+        return productName;
+    }
+
+    /**
+     * @param productName the productName to set
+     */
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    /**
+     * @return the qty
+     */
+    public double getQty() {
+        return qty;
+    }
+
+    /**
+     * @param qty the qty to set
+     */
+    public void setQty(double qty) {
+        this.qty = qty;
+    }
+
+    /**
+     * @return the price
+     */
+    public double getPrice() {
+        return price;
+    }
+
+    /**
+     * @param price the price to set
+     */
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
